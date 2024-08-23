c56c4eee2252a4944495df518039e98c1a24810f
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-08-23 16:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImages(img){
+	return new Promise((resolve, reject)=>{
+		const image = new Image();
+	image.src=img.url;
+
+	image.onload = ()=>{
+		resolve(image)
+	}
+	image.onerror = () =>{
+		reject("Loading error");
+	}
+	}) 
+}
+
+
+const imagePromises = images.map(downloadImages);
+btn.addEventListener("click",()=>{
+	output.innerHTML = "";
+	Promise.all(imagePromises).then((imgs)=>{
+		imgs.forEach((i)=>{
+			output.append(i)
+		})
+	})
+	.catch((err)=>{
+		console.log(`Failed to load image's URL: ${imgs.url}`)
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
